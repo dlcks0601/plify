ac7ef3d2955eb5a7ee1d1530537275a39dcb2cdc
leechan <dlcks0601@gmail.com>
2025-03-12 21:42:28 +0900

    [Fix] csp 제거

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   eslint: {
@@ -11,21 +10,6 @@ const nextConfig = {
       'image-cdn-ak.spotifycdn.com',
       'image-cdn-fa.spotifycdn.com',
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            // inline script를 허용하거나 필요한 nonce/해시를 추가할 수 있음
-            value:
-              "default-src 'self'; script-src 'self' https://sdk.scdn.co 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
-          },
-        ],
-      },
-    ];
   },
 };
 
